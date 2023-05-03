844a14c15f07c72380e9afe6e1cd07291751c0be
jeonghaebin <101246394+jeonghaebin@users.noreply.github.com>
2023-05-03 11:28:17 +0900

    해빈

---
+++ src/component/header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Data from "./date";
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="moon">
+                <div className="small-moon">
+                    <Data/>
+                </div>
+                
+            </div>
+            <div className="login">
+                <img className="log" src="./img/login.png" ></img>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
