d3754ea692523d2bfcb4a3671ae9fed0e4b562e5
jiae <yja2397@gmail.com>
2023-05-15 10:23:58 +0900

    월요일 띄어쓰기

--- src/component/header.js
+++ src/component/header.js
@@ -2,20 +2,17 @@ import React from "react";
 import Data from "./date";
 
 const Header = () => {
-    return (
-        <div className="header">
-            <div className="moon">
-                <div className="small-moon">
-                    
-                </div>
-                <Data/>
-                
-            </div>
-            <div className="login">
-                <img className="log" src="./img/login.png" ></img>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="header">
+      <div className="moon">
+        <div className="small-moon"></div>
+        <Data />
+      </div>
+      <div className="login">
+        <img className="log" src="./img/login.png"></img>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
